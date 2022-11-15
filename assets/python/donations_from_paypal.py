05803008a8d7687250fe87bf246f0a818e6cc641
Alex <alexhg15@gmail.com>
2022-11-15 17:48:14 -0500

    python script to get current donations from paypal

---
+++ assets/python/donations_from_paypal.py
@@ -0,0 +1,17 @@
+import re
+import requests
+import yaml
+from bs4 import BeautifulSoup
+
+SETTINGS = "_data/settings.yml"
+PATTERN = "\"campaignTotalAmountRaised\":\".*\""
+
+with open(SETTINGS, 'r') as f:
+    settings = yaml.safe_load(f)
+paypal_url = settings["donations"]["paypal_url"]
+html_r = requests.get(paypal_url, auth=('user', 'pass'))
+soup = BeautifulSoup(html_r.text, features="html.parser")
+re_str = re.findall(re.compile(PATTERN), str(soup.body))[0]
+donation_str = re_str.split(",")[0].split(":")[1].replace("\"", "")
+donation = float(donation_str)
+import ipdb; ipdb.set_trace()
